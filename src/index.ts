006e7dd858dbb6dacc83cb5d452a885c6023c7b0
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2020-01-06 09:30:29 +0000

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export { GLOBAL_INJECTOR } from './constants/global-injector.constant';
+export { Inject } from './decorators/inject/inject.decorator';
+export { Injectable } from './decorators/injectable/injectable.decorator';
 export { Injector } from './classes/injector/injector';
 export { Token } from './classes/token/token';
