22d5af0cf2fe9eccc034341aeb0a50e95d475eaa
Anthony Cleaver <Anthony.Cleaver@mhrglobal.com>
2024-10-14 22:09:28 +0100

    Remove cleavera libs

--- src/classes/injector/injector.ts
+++ src/classes/injector/injector.ts
@@ -1,46 +1,43 @@
-import { Maybe } from '@cleavera/types';
-import { isNull } from '@cleavera/utils';
-
 import { Token } from '../token/token';
 
 const noop: () => null = (): null => {
-    return null;
+  return null;
 };
 
 export class Injector {
-    private _registry: any;
+  private _registry: any;
 
-    constructor() {
-        this.clear();
-    }
+  constructor() {
+    this.clear();
+  }
 
-    public setValue<T>(token: Token<T>, value: T): void {
-        this._set(token, () => {
-            return value;
-        });
-    }
+  public setValue<T>(token: Token<T>, value: T): void {
+    this._set(token, () => {
+      return value;
+    });
+  }
 
-    public setFactory<T>(token: Token<T>, factory: () => T): void {
-        let value: Maybe<T> = null;
+  public setFactory<T>(token: Token<T>, factory: () => T): void {
+    let value: T | null = null;
 
-        this._set(token, () => {
-            if (isNull(value)) {
-                value = factory();
-            }
+    this._set(token, () => {
+      if (value === null) {
+        value = factory();
+      }
 
-            return value;
-        });
-    }
+      return value;
+    });
+  }
 
-    public get<T>(token: Token<T>): Maybe<T> {
-        return (this._registry[token.symbol as any] ?? noop)();
-    }
+  public get<T>(token: Token<T>): T | null {
+    return (this._registry[token.symbol as any] ?? noop)();
+  }
 
-    public clear(): void {
-        this._registry = {};
-    }
+  public clear(): void {
+    this._registry = {};
+  }
 
-    private _set<T>(token: Token<T>, func: () => T): void {
-        this._registry[token.symbol as any] = func;
-    }
+  private _set<T>(token: Token<T>, func: () => T): void {
+    this._registry[token.symbol as any] = func;
+  }
 }
